b47289d841b71a7cb6d3e3c3c78fcd5088cbd8d2
wangkaichuang <wkc@yx-catl.com>
2024-08-21 17:45:33 +0800

    可以保存整个文件夹的检测结果

--- my_data/code/detect.py
+++ my_data/code/detect.py
@@ -8,11 +8,6 @@ model = YOLO('../weights/yolov8s.pt')
 # Specify the input and output directories
 input_dir = '../coco128/images/train2017'
 
-# Process each image in the input directory
-for img_file in os.listdir(input_dir):
-    if img_file.endswith(('png', 'jpg', 'jpeg', 'bmp', 'tiff')):
-        img_path = os.path.join(input_dir, img_file)
-        img = cv2.imread(img_path)
-        results = model(img,save=True,save_frames=True)
+results = model(input_dir,save=True)
 
 print("Batch processing completed.")
--- my_data/code/seg.py
+++ my_data/code/seg.py
@@ -8,12 +8,7 @@ model = YOLO('../weights/yolov8s-seg.pt')
 # Specify the input and output directories
 input_dir = '../coco128-seg/images/train2017'
 
-# Process each image in the input directory
-for img_file in os.listdir(input_dir):
-    if img_file.endswith(('png', 'jpg', 'jpeg', 'bmp', 'tiff')):
-        img_path = os.path.join(input_dir, img_file)
-        img = cv2.imread(img_path)
-        results = model(img,save=True)
+results = model(input_dir,save=True)
         
         # output_path = os.path.join(output_dir, img_file)
         # print(results)

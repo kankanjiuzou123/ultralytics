bc821e0b5fd86bee07e431e6b03b3b818ada9198
wangkaichuang <18530121558@163.com>
2024-08-22 17:57:17 +0800

    add no augment

--- baitiao/code/train_baitiao.py
+++ baitiao/code/train_baitiao.py
@@ -2,4 +2,6 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO("../../my_data/weights/yolov8s-seg.pt")
-results = model.train(data="../cfg/baitiao.yaml", epochs=600, imgsz=640,batch=8,lr0=0.001,optimizer='Adam')
+results = model.train(data="../cfg/baitiao.yaml", epochs=100, imgsz=640,batch=8,lr0=0.001,optimizer='Adam',augment=False,auto_augment='',
+                      hsv_h=0.0, hsv_s=0.0, hsv_v=0.0, degrees=0.0, translate=0.0, scale=0.0, shear=0.0, 
+                      perspective=0.0, flipud=0.0,fliplr=0.0, mosaic=0.0, mixup=0.0)

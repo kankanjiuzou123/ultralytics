f2abf24a86049887c010b08a0ec158f91b35b44c
wangkaichuang <18530121558@163.com>
2024-08-22 13:20:20 +0800

    change some hypermeter

--- baitiao/code/train_baitiao.py
+++ baitiao/code/train_baitiao.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO("../../my_data/weights/yolov8s-seg.pt")
-results = model.train(data="../cfg/baitiao.yaml", epochs=600, imgsz=640,batch=8)
+results = model.train(data="../cfg/baitiao.yaml", epochs=600, imgsz=640,batch=8,lr0=0.001,optimizer='Adam')
